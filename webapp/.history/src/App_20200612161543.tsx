2fe75005bc6d180213485d11150171eae453b3e3
DavidSamerson <david@moldsoft.com.br>
2020-06-12 20:11:30 -0300

    Novas modificações

---
+++ webapp/.history/src/App_20200612161543.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <p>
+          Hello Word
+        </p>
+  );
+}
+
+export default App;
---
+++ webapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+//JSX -> sintaxe de xml dentro do javascript
+
+function App() {
+  return (
+        <p>
+          Hello Word
+        </p>
+  );
+}
+
+export default App;
